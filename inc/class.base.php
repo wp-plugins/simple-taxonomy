c14a8e11b595ce98347c41649f16f762b8f05625
momo360modena <momo360modena@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-24 14:18:26 +0000

    release Simple Taxonomy 3.1
    git-svn-id: https://plugins.svn.wordpress.org/simple-taxonomy/tags/3.1@350434 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/class.base.php
@@ -0,0 +1,19 @@
+<?php
+class SimpleTaxonomy_Base {
+	/**
+	 * Empty function for callback install
+	 *
+	 * @return void
+	 * @author Amaury Balmer
+	 */
+	function activate() {}
+	
+	/**
+	 * Empty function for callback uninstall
+	 *
+	 * @return void
+	 * @author Amaury Balmer
+	 */
+	function deactivate() {}
+}
+?>
